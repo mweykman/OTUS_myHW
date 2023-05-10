a505aa22d30f7d67b288a5497695f87dd1ae2c5a
Mikhail <mweykman@gmail.com>
2023-05-10 20:59:12 +0700

    PAM works

--- les28/script.sh
+++ les28/script.sh
@@ -5,5 +5,7 @@ groupadd -f admin
 usermod otusadm -aG admin && usermod root -aG admin && usermod vagrant -aG admin
 LC_TIME=en_US.UTF-8
 cd /vagrant
-cp -f sshd /etc/pam.d
+#cp -f sshd /etc/pam.d
+echo "auth required pam_exec.so /usr/local/bin/login.sh" >> /etc/pam.d/sshd
 cp -f login.sh /usr/local/bin
+sudo date 082712302022.00
