5f553bca48ac06d02ea0a654c7937f6afe0e33e0
Mikhail <mweykman@gmail.com>
2023-05-10 20:35:04 +0700

    PAM hw start...

--- les28/script.sh
+++ les28/script.sh
@@ -4,3 +4,6 @@ echo "Otus2022!" | passwd --stdin otusadm && echo "Otus2022!" | passwd --stdin o
 groupadd -f admin
 usermod otusadm -aG admin && usermod root -aG admin && usermod vagrant -aG admin
 LC_TIME=en_US.UTF-8
+cd /vagrant
+cp -f sshd /etc/pam.d
+cp -f login.sh /usr/local/bin
