abfddff42eff9eb207cba649f753cd1ac5a09176
Mikhail <mweykman@gmail.com>
2023-08-05 00:35:46 +0700

    backup again

--- les38/script_client.sh
+++ les38/script_client.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 echo "инициализируем репозиторий borg на сервере backup"
-borg init --encryption=repokey borg@192.168.56.10:/var/backup/
+# С шифрованием, запросит пароль
+#borg init --encryption=repokey borg@192.168.56.10:/var/backup/
+# Без ширования, не запрашивает пароль
+borg init -e none borg@192.168.56.10:/var/backup/
 echo "запускаем подготовленный ранее ancible'ом сервис для автоатического создания бэкапов"
 sudo systemctl enable borg-backup.timer
 sudo systemctl start borg-backup.timer
